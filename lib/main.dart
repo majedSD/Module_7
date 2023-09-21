9f9660555b0cc2b2c615320327423ec71dd3611f
Ekushey Computer <majedulislam117@gmail.com>
2023-09-21 22:57:37 +0600

    CupertinoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+/**
+ * This programme is about coupatino and how it works
+ */
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +13,7 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
+   return CupertinoApp(
      home: HomePage(),
    );
   }
@@ -25,9 +29,60 @@ class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Icon(CupertinoIcons.home),
+        middle: Text('Home page'),
+        trailing: CupertinoSwitch(
+          onChanged: (bool onChange){
 
+          }, value: true,
+        ),
+      ) ,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Text("Apple work page"),
+            Text('Apple work page'),
+            Text('Apple work page'),
+            CupertinoButton.filled(
+                child: Text('Enter the button') ,
+                onPressed: (){}
+            ),
+            CupertinoTextField(),
+            CupertinoTabBar(items: [
+              BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Person'),
+              BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message'),
+              BottomNavigationBarItem(icon: Icon(Icons.email),label: 'Email'),
+            ])
+          ],
+        ),
+      ),
     );
   }
-
 }
+/*
+        ///This programme is about Fractionaly SizedBox
+        FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.3,
+          child: Container(
+            color: Colors.pink,
+          ),
+        ),
+      ),
+      ///This programme is about AspectRatio and how aspect ratio work
+      Container(
+        height: 300,
+        width: double.infinity,
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: AspectRatio(
+          aspectRatio: 16/9,
+       child: Container(
+          color: Colors.green,
+         child: Text('Hello world'),
+        ),
+      ),
+      ),*/
