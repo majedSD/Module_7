7bf08a7fa42c17776776fbb03ac37f0b911a21d4
Ekushey Computer <majedulislam117@gmail.com>
2023-09-22 00:34:13 +0600

    CupertinoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+/**
+ * This programme is about coupatino and how it works
+ */
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -9,9 +12,9 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     home: HomePage(),
-   );
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 
 }
@@ -19,7 +22,7 @@ class HomePage extends StatelessWidget{
   const HomePage({super.key});
   MySnackBar(message,context){
     return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
+      SnackBar(content: Text(message)),
     );
   }
 
@@ -27,7 +30,55 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
 
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Switch(value: true, onChanged: (bool value){}),
+            ElevatedButton(onPressed: (){
+              showDialog(context: context,
+                  barrierDismissible: false,
+                  builder: (context){
+                   return AlertDialog(
+                     title: Text('Alert!'),
+                     content: Text('Please enter the setting screen'),
+                     actions: [
+                       TextButton(onPressed: (){}, child: Text('Yes')),
+                       TextButton(onPressed: (){
+                         Navigator.pop(context);
+                       }, child: Text('No'))
+                     ],
+                   );
+                  }
+              );
+            }, child: Text('Enter Name')),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              showModalBottomSheet(
+                  isDismissible: false,
+                  backgroundColor: Colors.pink,
+                  barrierColor: Colors.black26,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    )
+                    
+                  ),
+                  context: context,
+                  builder: (context){
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('This is bottom sheet'),
+                    ],
+                  ),
+                );
+                  }
+                  );
+            }, child: Text('Show bottom sheet')),
+          ],
+      ),
     );
   }
-
 }
